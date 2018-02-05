4215811bbe75ed0bfc3585cac621307f69a3e153
Jacob Reed <jacob.russell.reed@gmail.com>
2018-02-04 20:51:18 -0800

    Added stuff

--- UrbanParksSystem/src/DataStore.java
+++ UrbanParksSystem/src/DataStore.java
@@ -1,4 +1,15 @@
+import java.io.Serializable;
 
-public class DataStore {
+/*
+ * Class for storing persistent data
+ * Username with permission level
+ * Jobs
+ */
+public class DataStore implements Serializable{
 
+	/**Generated SUID**/
+	private static final long serialVersionUID = -7839681454166039293L;
+	public String myUsername;
+	public int myPermission;
+	//public List<Job> = new List<Job>();
 }
