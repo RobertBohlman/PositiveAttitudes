1b29aaf6a38030c33130429dc566a1fcf913f3db
Jacob Reed <jacob.russell.reed@gmail.com>
2018-02-04 21:33:19 -0800

    Stuff, not done

--- UrbanParksSystem/src/DataStore.java
+++ UrbanParksSystem/src/DataStore.java
@@ -1,3 +1,8 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -13,4 +18,46 @@ public class DataStore implements Serializable{
 	public String myUsername;
 	public int myPermission;
 	public ArrayList<Job> myJobs = new ArrayList<Job>();
+	
+	/**
+	 * Constructor.
+	 * @param theUsername Username
+	 * @param thePermission Permission Rank (1-3)
+	 * @param theJobs List of jobs pertaining to user
+	 */
+	public DataStore(final String theUsername, final int thePermission,
+						final ArrayList<Job> theJobs) {
+		myUsername = theUsername;
+		myPermission = thePermission;
+		myJobs = theJobs;
+	}
+	
+	/**
+	 * Stores DataStore object
+	 */
+	public void Store() {
+		try {
+			FileOutputStream fileOut = new
+					FileOutputStream("/tmp/" + myUsername + ".ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(myUsername);
+			out.writeObject(myPermission);
+			out.writeObject(myJobs);
+			fileOut.close();
+			System.out.println("Serliazed data is saved to /tmp/" + myUsername + ".ser");
+		}catch (IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public void Load(final String theUsername) {
+		try {
+			FileInputStream fileIn = new FileInputStream("/tmp/" + theUsername + ".ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			//TODO
+			
+		}
+	}
+	
+	
 }
