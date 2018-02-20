797c89a2132780079fea43a6052d15613ff011e6
RobertBohlman <robertbohlman@hotmail.com>
2018-02-20 13:22:14 -0800

    Added unit tests for number 7

--- UrbanParksSystem/src/ParkManagerTest.java
+++ UrbanParksSystem/src/ParkManagerTest.java
@@ -9,24 +9,42 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-class ParkManagerTest {
+public class ParkManagerTest {
 
-//	private static final int MAX_NUM_JOBS = 20;
-//	
-//	private List<Job> fewerThanMaxJobs;
-//	private List<Job> maxJobs;
-//	private Job submittableJob;
-//	private ParkManager managerUser;
-//	
-//	@Before
-//	void setUp() throws Exception {
-//		fewerThanMaxJobs = new ArrayList<Job>();
-//		maxJobs = new ArrayList<Job>();
-//		submittableJob = new Job();
-//		managerUser = new ParkManager("manager1", 1, "manager@parks.org");
-//		
-//	}
-//	
+	private static final int MAX_NUM_JOBS = 20;
+	
+	private List<Job> fewerThanMaxJobs;
+	private List<Job> maxJobs;
+	private Job submittableValidJob;
+	private Job submittableInvalidJob;
+	private ParkManager managerUser;
+	private SystemData UrbanParksSystem;
+	
+	@Before
+	public void setUp() throws Exception {
+		UrbanParksSystem = new SystemData();
+		fewerThanMaxJobs = new ArrayList<Job>();
+		maxJobs = new ArrayList<Job>();
+		submittableValidJob = new Job("Test Job", "2018/02/26", "", 2, "", "", 1);
+		submittableInvalidJob = new Job("Test Job", "2018/10/26", "", 2, "", "", 1);
+		managerUser = new ParkManager("manager1", 1, "manager@parks.org");
+		UrbanParksSystem.addUser(managerUser);
+		UrbanParksSystem.setCurrentUser(managerUser.getUserName());
+		
+	}
+	
+	@Test
+	public void submitValidJob_ManagerRemembersJob_returnTrue() {
+		UrbanParksSystem.submitJob(submittableValidJob);
+		assertTrue(managerUser.getJobs().contains(submittableValidJob.myTitle.hashCode()));
+	}
+	
+	@Test
+	public void submitInvalidJob_ManagerRemembersJob_returnFalse() {
+		UrbanParksSystem.submitJob(submittableInvalidJob);
+		assertFalse(managerUser.getJobs().contains(submittableInvalidJob.myTitle.hashCode()));
+	}
+	
 //	@Test
 //	void submitJob_tooManyJobs_returnFalse() { 
 //		for (int i = 0; i < 30; i++) {
@@ -53,31 +71,31 @@ class ParkManagerTest {
 //		UrbanParksTerminal.addJob(submittableJob);
 //		assertTrue(managerUser.getJobs().contains(submittableJob));
 //	}
-//
-//	@Test
-//	/*
-//	 * Tests return of true if job is less than maximum
-//	 * of 3 days, testing using one less (2)
-//	 */
-//	void submitJob_durationLessThanMax_returnTrue() {
-//		//assertEquals(//TODO check job length, true);
-//	}
-//	
-//	@Test
-//	/*
-//	 * Tests return of true if job number of days
-//	 * is equal to the exact max
-//	 */
-//	void submitJob_durationEqualsMax_returnTrue() {
-//		//assertEquals(//TODO checkJob Length, true);
-//	}
-//	
-//	@Test
-//	/*
-//	 * Tests return of false if job number of days is > max
-//	 * Using one greater than max (4)
-//	 */
-//	void submitJob_durationGreaterThanMax_returnFalse() {
-//		//assertEquals(//TODO check job length, false);
-//	}
+
+	@Test
+	/*
+	 * Tests return of true if job is less than maximum
+	 * of 3 days, testing using one less (2)
+	 */
+	public void submitJob_durationLessThanMax_returnTrue() {
+		//assertEquals(//TODO check job length, true);
+	}
+	
+	@Test
+	/*
+	 * Tests return of true if job number of days
+	 * is equal to the exact max
+	 */
+	public void submitJob_durationEqualsMax_returnTrue() {
+		//assertEquals(//TODO checkJob Length, true);
+	}
+	
+	@Test
+	/*
+	 * Tests return of false if job number of days is > max
+	 * Using one greater than max (4)
+	 */
+	public void submitJob_durationGreaterThanMax_returnFalse() {
+		//assertEquals(//TODO check job length, false);
+	}
 }
